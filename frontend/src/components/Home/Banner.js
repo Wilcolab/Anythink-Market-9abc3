50921b12c92fe98503e3d107855123c5e6bf4c2f
Richd0tcom <tuberich0@gmail.com>
2022-09-27 01:09:14 +0100

    feat: hide search box

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import Search from "./Search";
 
 const Banner = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <Search />
+          <span id="get-part" onClick={(e) => setIsOpen(true)}>
+            A place to get
+          </span>
+          {isOpen ? <Search /> : null}
           <span> the cool stuff.</span>
         </div>
       </div>
