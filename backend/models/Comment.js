5d1b391228a1d1fb5b82e4796a4267ed698fd340
Richd0tcom <tuberich0@gmail.com>
2022-10-17 21:06:51 +0100

    data seeding

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -4,19 +4,20 @@ var CommentSchema = new mongoose.Schema(
   {
     body: String,
     seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    item: { type: mongoose.Schema.Types.ObjectId, ref: "Item" }
+    item: { type: mongoose.Schema.Types.ObjectId, ref: "Item" },
   },
   { timestamps: true }
 );
 
 // Requires population of seller
-CommentSchema.methods.toJSONFor = function(user) {
+CommentSchema.methods.toJSONFor = function (user) {
   return {
     id: this._id,
     body: this.body,
     createdAt: this.createdAt,
-    seller: this.seller.toProfileJSONFor(user)
+    seller: this.seller.toProfileJSONFor(user),
   };
 };
 
-mongoose.model("Comment", CommentSchema);
+const Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,57 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { faker } = require("@faker-js/faker");
+mongoose.connect(process.env.MONGODB_URI);
+
+const User = require("../models/User");
+const Item = require("../models/Item");
+const Comment = require("../models/Comment");
+
+const createUser = () => {
+  return {
+    username: faker.database.mongodbObjectId(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    bio: faker.lorem.paragraph(),
+    image: faker.image.avatar(),
+  };
+};
+
+const hundredUsers = faker.helpers.uniqueArray(createUser, 100);
+
+const createItem = () => {
+  const title = faker.commerce.productName() + faker.datatype.uuid();
+  return {
+    title,
+    slug: faker.helpers.slugify(title),
+    description: faker.commerce.productDescription(),
+    image: faker.image.abstract(),
+  };
+};
+
+const hundredItems = faker.helpers.uniqueArray(createItem, 100);
+
+const createComment = () => {
+  return {
+    body: faker.lorem.paragraph(),
+    seller: User.findOne()._id,
+    item: Item.findOne()._id,
+  };
+};
+
+const hundredComments = faker.helpers.uniqueArray(createComment, 100);
+
+const seed = async () => {
+  await User.deleteMany({});
+  await Item.deleteMany({});
+  await Comment.deleteMany({});
+  await User.insertMany(hundredUsers);
+  await Item.insertMany(hundredItems);
+  await Comment.insertMany(hundredComments);
+  console.log("Database seeded");
+  mongoose.connection.close();
+  process.exit();
+};
+
+seed();
